f06bad5086c42f5d54a3af4598bda21d75869873
harshrs21 <58426854+harshrs21@users.noreply.github.com>
2019-12-09 17:27:06 +0530

    Add files via upload

---
+++ Assignment 3/Q1.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+ls = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+ln = [5,7,8,3,5,6,3,7,2,12,5,7,2,6,9,2]
+plt.plot(ls,ln)
+plt.title('Epic Info')
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+plt.show()
